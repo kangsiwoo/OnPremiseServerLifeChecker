6d4cd64d55ea94dbc1359ab2406efb65ce1106c8
kangsiwoo <bssmkangsiwoo@gmail.com>
2024-10-19 23:11:11 +0900

    feat(root) :: Build Spring
    jar 17 gradle

---
+++ src/main/java/com/clxxthyng/OPSLC/OpslcApplication.java
@@ -0,0 +1,13 @@
+package com.clxxthyng.OPSLC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpslcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpslcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clxxthyng/OPSLC/OpslcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clxxthyng.OPSLC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpslcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
